9ef19f58ce67990b02e6d1dbd6ab89cae3a3bd21
Giridhar Addagalla <giridhar196@gmail.com>
2021-03-29 13:37:49 -0500

    updated app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/series',serialsRouter);
+app.use('/serials',serialsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
