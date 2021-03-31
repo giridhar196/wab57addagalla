982bcb3995943ea953aeb257c74666f681f5e1fb
Giridhar Addagalla <giridhar196@gmail.com>
2021-03-31 18:57:29 -0500

    updated the app

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var serialsRouter = requiregit('./routes/serials');
+var serialsRouter = require('./routes/serials');
 
 var app = express();
 
